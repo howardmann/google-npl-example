61490fd03189764bdb1d75a033930eda246bb773
howardmann <howardmann27@gmail.com>
2017-10-23 13:21:41 +1100

    Change dirname

--- google.js
+++ google.js
@@ -8,7 +8,7 @@ const language = require('@google-cloud/language');
 
 // Instantiates a client
 const client = new language.LanguageServiceClient({
-  keyFilename: './secrets/google.json'
+  credentials: require('./secrets/google')
 });
 
 // // The text to analyze
