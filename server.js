2e6a89306cf4afd4fee07f0bfdb8de7b7b7974fb
howardmann <howardmann27@gmail.com>
2017-10-23 12:04:34 +1100

    Update client side

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 require('dotenv').config();
 
+// Dependencies
 var express = require('express');
-var app = express();
+var bodyParser = require('body-parser');
 var google = require('./google')
 
+var app = express();
+
+// Set view engine to handlebars
+app.engine('hbs', require('express-handlebars')({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layouts/'}));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'hbs');
+
+// Parse form data req.body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get('/', (req, res, next) => {
-  res.send('harooo');
+  res.render('home');
 })
 
 app.get('/analyze/:text', (req, res, next) => {
@@ -15,6 +27,33 @@ app.get('/analyze/:text', (req, res, next) => {
     })
     .catch(err => {
       res.send(err)
+    })
+})
+
+app.post('/googlesentiment', (req, res, next) => {
+  let text = req.body.text;
+  let getColor = (score) => {
+    if (score >= 0.25) {
+      return 'green'
+    } else if (score >= -0.25 && score < 0.25) {
+      return 'orange'
+    } else {
+      return 'red'
+    }
+  }
+
+  google.analyze(text)
+    .then(results => {
+      let score = results[0].documentSentiment.score;
+      let magnitude = results[0].documentSentiment.magnitude;
+      let color = getColor(score)
+      res.render('home', {
+        textinput: text,
+        textresults: JSON.stringify(results),
+        score,
+        magnitude,
+        color
+      });
     })
 })
 
